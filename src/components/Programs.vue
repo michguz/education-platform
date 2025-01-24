a61aa34000fa9f3067bda64cead4024a4bb450ae
null <null>
2025-01-24 14:29:58 +1100

    Added bootstrap

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -1,1 +1,17 @@
-<template><div><h1>About Us</h1></div></template>
+<template>
+    <div>
+        <h2>Bootstrap</h2>
+    </div>
+
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+    </div>
+
+</template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 
 
+
 createApp(App)
     .use(router)
     .mount('#app')
